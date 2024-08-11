c465afd4a08b778d72bfb33296c6bdcad9e37945
ankitmishra42 <ankitmishra6221@gmail.com>
2024-08-11 23:43:38 +0530

    first commit

---
+++ src/app/MyComponents/workout-list/workout-list.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-workout-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './workout-list.component.html',
+  styleUrl: './workout-list.component.css'
+})
+export class WorkoutListComponent {
+  q = "Abhay"
+  p = "Divyansh"
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { InputFormComponent } from './MyComponents/input-form/input-form.component';
+import { WorkoutListComponent } from './MyComponents/workout-list/workout-list.component';
+import { WorkoutProgressComponent } from './MyComponents/workout-progress/workout-progress.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, InputFormComponent, WorkoutListComponent, WorkoutProgressComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'my-app';
+  myName = 'Ankit Mishra'
+  // constructor(){
+  //   setTimeout(() => {
+  //     this.myName ='Jay ShiyaRam';
+  //   }, 2000);
+  // }
 }
